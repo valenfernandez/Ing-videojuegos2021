7e48bc41d25982e25365cad566aa971fa24b260f
valenfernandez <valenfernandez@users.noreply.github.com>
2021-06-23 08:31:04 -0300

    Músicos muestran su puntaje

--- producto/Divertango-v1/scripts/Musicos.gd
+++ producto/Divertango-v1/scripts/Musicos.gd
@@ -3,6 +3,17 @@ extends Node2D
 func _ready():
 	var teatro = DiccionarioNiveles.nivel_actual
 	$AnimatedSprite.frame = teatro - 1
+	var partidas_ant = Global.partidas
+	for enc in partidas_ant:
+		if(enc != null && enc.teatro == teatro):
+			if (enc.musico == 1):
+				$LabelPuntaje1.text = str(enc.highscore)
+			elif (enc.musico == 2):
+				$LabelPuntaje2.text = str(enc.highscore)
+			elif (enc.musico == 3):
+				$LabelPuntaje3.text = str(enc.highscore)
+	
+	
 
 func _on_musico1_pressed():
 	DiccionarioNiveles.set_musico(1)
